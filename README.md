909b0db143e2f81542908fac936ff5a3a3ca93b5
zxhrah <zahrahrahman7@gmail.com>
2023-12-15 22:09:05 +0000

    myforum v5

---
+++ README.md
@@ -0,0 +1,8 @@
+Once the files are installed open up vscode and open up the terminal
+run the following commands:
+npm install express
+npm install ejs
+npm install mysql
+
+to run the index.js type the command:
+node index.js
--- insert_test_data.sql
+++ insert_test_data.sql
@@ -5,5 +5,3 @@ INSERT INTO `membership` VALUES (1,1),(1,2),(1,1),(1,1),(1,1),(2,1),(1,1),(1,2),
 INSERT INTO `topic` VALUES (1,'cats vs dogs'),(2,'coffee');
 
 INSERT INTO `user` VALUES (1,'hesita','hesita','hesita@gmail.com'),(2,'lats','lats','lats@gmail.com');
-
-
